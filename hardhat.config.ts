b4f33db07c11d47556b2567daec0e07bbf740454
numoonchld <raghavendra.saralaya@gmail.com>
2022-10-09 18:28:51 +0530

    hardhat env setup complete

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig, task } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.17',
+}
+
+export default config
+
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
